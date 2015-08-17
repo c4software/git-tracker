97ea9344ce4606f10d72b39f8b0eb89e60c350b4
Valentin Brosseau <c4software@gmail.com>
2015-08-17 22:02:40 +0200

    mend

--- git_tracker.py
+++ git_tracker.py
@@ -1,8 +1,22 @@
-from extended_BaseHTTPServer import serve,route
+import subprocess
+import json
 
-@route("/get_worker",["GET"])
-def main(**kwargs):
-    return "Hello Wrapper"
+from extended_BaseHTTPServer import serve,route
+from jinja_helper import render
+
+@route("/",["GET"])
+def home(**kwargs):
+    return render("liste.html")
+
+# API
+@route("/get_author",["GET"])
+def get_author(**kwargs):
+    command = "git log --all --format='%aN <%cE>' | sort -u"
+    try:
+        output = subprocess.check_output(command, shell=True).splitlines()
+        return json.dumps(output)
+    except:
+        return json.dumps([])
 
 if __name__ == '__main__':
     serve(ip="0.0.0.0", port=5000)
